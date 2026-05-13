2c812eb9150ffefd74d880b1a3c17a3abbfd6e86
Axel225-CI <Axelworld273@gmail.com>
2026-05-13 17:07:19 +0000

    troisieme test

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -28,3 +28,10 @@ void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
 
 void main() {
 }
+@Test
+public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+    BookShelf shelf = new BookShelf();
+    shelf.add();
+    List<String> books = shelf.books();
+    assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+}
