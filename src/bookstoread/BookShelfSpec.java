5e822cdc5786a1242fb140a27ef279bc6e098d0d
Axel225-CI <Axelworld273@gmail.com>
2026-05-13 17:05:21 +0000

    Deuxieme test

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,18 +1,30 @@
-package bookstoread;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import bookstoread.BookShelf;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class BookShelfSpec {
+@Nested
+class bookShelfSpec {
     @Test
-    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-        BookShelf shelf = new BookShelf();
-        shelf.add("Effective Java");
-        shelf.add("Code Complete");
+    public void shelfEmptyWhenNoBookAdded() throws Exception {
+        bookstoread.BookShelf shelf = new bookstoread.BookShelf();
         List<String> books = shelf.books();
-        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
+
+}
+@Test
+void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+    BookShelf shelf = new BookShelf();
+    shelf.add("Effective Java");
+    shelf.add("Code Complete");
+    List<String> books = shelf.books();
+    assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+}
+
+void main() {
 }
